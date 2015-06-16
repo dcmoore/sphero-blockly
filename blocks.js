3e9ebcd3a0679eb6fdbe4e0d906718f0d0d9b508
Dave Moore <dave@8thlight.com>
2015-06-15 19:15:33 -0700

    Added other directional blocks and allowed chaining of them

--- blocks.js
+++ blocks.js
@@ -4,7 +4,48 @@ Blockly.Blocks['roll_forward'] = {
     this.setColour(120);
     this.appendValueInput("speed")
         .setCheck("Number")
-        .appendField("Speed");
+        .appendField("Forward");
+    this.setPreviousStatement(true, ["roll_forward", "roll_reverse", "roll_left", "roll_right"]);
+    this.setNextStatement(true, ["roll_forward", "roll_reverse", "roll_left", "roll_right"]);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['roll_reverse'] = {
+  init: function() {
+    this.setHelpUrl('http://www.example.com/');
+    this.setColour(120);
+    this.appendValueInput("speed")
+        .setCheck("Number")
+        .appendField("Reverse");
+    this.setPreviousStatement(true, ["roll_forward", "roll_reverse", "roll_left", "roll_right"]);
+    this.setNextStatement(true, ["roll_forward", "roll_reverse", "roll_left", "roll_right"]);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['roll_left'] = {
+  init: function() {
+    this.setHelpUrl('http://www.example.com/');
+    this.setColour(120);
+    this.appendValueInput("speed")
+        .setCheck("Number")
+        .appendField("Left");
+    this.setPreviousStatement(true, ["roll_forward", "roll_reverse", "roll_left", "roll_right"]);
+    this.setNextStatement(true, ["roll_forward", "roll_reverse", "roll_left", "roll_right"]);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['roll_right'] = {
+  init: function() {
+    this.setHelpUrl('http://www.example.com/');
+    this.setColour(120);
+    this.appendValueInput("speed")
+        .setCheck("Number")
+        .appendField("Right");
+    this.setPreviousStatement(true, ["roll_forward", "roll_reverse", "roll_left", "roll_right"]);
+    this.setNextStatement(true, ["roll_forward", "roll_reverse", "roll_left", "roll_right"]);
     this.setTooltip('');
   }
 };
--- javascript.js
+++ javascript.js
@@ -1,6 +1,27 @@
 Blockly.JavaScript['roll_forward'] = function(block) {
   var value_speed = Blockly.JavaScript.valueToCode(block, 'speed', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = 'alert('+value_speed+');';
+  var code = 'alert("forward '+value_speed+'");';
+  return code;
+};
+
+Blockly.JavaScript['roll_reverse'] = function(block) {
+  var value_speed = Blockly.JavaScript.valueToCode(block, 'speed', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'alert("reverse '+value_speed+'");';
+  return code;
+};
+
+Blockly.JavaScript['roll_left'] = function(block) {
+  var value_speed = Blockly.JavaScript.valueToCode(block, 'speed', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'alert("left '+value_speed+'");';
+  return code;
+};
+
+Blockly.JavaScript['roll_right'] = function(block) {
+  var value_speed = Blockly.JavaScript.valueToCode(block, 'speed', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'alert("right '+value_speed+'");';
   return code;
 };
