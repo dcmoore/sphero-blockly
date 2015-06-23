6499fbe17ac8b9d7bbf92903b458fa288fd132d0
Dave Moore <dave@8thlight.com>
2015-06-22 17:35:32 -0700

    Added clojure to the party

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject sphero-blockly "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.3.1"]
+                 [ring/ring-defaults "0.1.2"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler sphero-blockly.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
