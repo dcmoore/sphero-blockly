f8ace7b79ec27ea99184488bbadfcefddb185c96
Zach Olauson <zmolauson@gmail.com>
2015-06-22 22:01:25 -0700

    Setup Blockly testing

---
+++ resources/public/spec/javascript_spec.js
@@ -0,0 +1,7 @@
+describe("Blockly.Javascript", function() {
+  describe("#roll_forward", function() {
+    it("should return its speed", function() {
+      var rollForward = Blockly.JavaScript["roll_forward"];
+    })
+  })
+})
