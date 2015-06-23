6f62e6791792c0e26acd19c5d35b6a6188e205c4
Dave Moore <dave@8thlight.com>
2015-06-22 17:51:08 -0700

    Serving Blockly editor through web-app

--- src/sphero_blockly/handler.clj
+++ src/sphero_blockly/handler.clj
@@ -1,11 +1,14 @@
 (ns sphero-blockly.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.resource :refer [wrap-resource]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+    (wrap-defaults site-defaults)
+    (wrap-resource "public")))
