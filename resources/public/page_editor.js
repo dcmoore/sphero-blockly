9ab566930d709bfa9247ad7ae09628b2f1021410
Zach Olauson <zmolauson@gmail.com>
2015-06-23 20:50:42 -0700

    Remove unused js file

--- resources/public/page_editor.js
+++
@@ -1,6 +0,0 @@
-Blockly.JavaScript['roll_forward'] = function(block) {
-  var value_speed = Blockly.JavaScript.valueToCode(block, 'speed', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'alert('+value_speed+');';
-  return code;
-};
