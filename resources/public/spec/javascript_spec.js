05f4808446fc6c6a7ad6e83ac030ef30cc057d93
Zach Olauson <zmolauson@gmail.com>
2015-06-23 19:27:51 -0700

    Add Commander tests

--- resources/public/spec/javascript_spec.js
+++ resources/public/spec/javascript_spec.js
@@ -1,8 +1,57 @@
+describe("Commander", function() {
+
+  var HttpClient = function() {};
+  HttpClient.prototype.get = function(url) {};
+
+  var httpClient;
+  var commander;
+
+  beforeEach(function() {
+    httpClient = new HttpClient();
+
+    spyOn(httpClient, "get");
+
+    commander = new Commander(httpClient);
+  })
+
+  describe("#forward", function() {
+    it("sends a http request to the server with the forward command an the given speed", function() {
+      commander.forward(10);
+
+      expect( httpClient.get ).toHaveBeenCalledWith("http://localhost:3000/command/forward?speed=10");
+    })
+  })
+
+  describe("#reverse", function() {
+    it("sends a http request to the server with the reverse command an the given speed", function() {
+      commander.reverse(10);
+
+      expect( httpClient.get ).toHaveBeenCalledWith("http://localhost:3000/command/reverse?speed=10");
+    })
+  })
+
+  describe("#left", function() {
+    it("sends a http request to the server with the left command an the given speed", function() {
+      commander.left(10);
+
+      expect( httpClient.get ).toHaveBeenCalledWith("http://localhost:3000/command/left?speed=10");
+    })
+  })
+
+  describe("#right", function() {
+    it("sends a http request to the server with the right command an the given speed", function() {
+      commander.right(10);
+
+      expect( httpClient.get ).toHaveBeenCalledWith("http://localhost:3000/command/right?speed=10");
+    })
+  })
+});
+
 describe("Blockly.Javascript", function() {
   describe("#number_value", function() {
 
     var numberValueBlock;
-    var numberValueBlockEvaluator
+    var numberValueBlockEvaluator;
 
     beforeEach(function() {
       numberValueBlock = new MockValueBlock();
@@ -13,7 +62,7 @@ describe("Blockly.Javascript", function() {
     it("pulls the input_value field from the block", function() {
       spyOn(numberValueBlock, "getFieldValue");
 
-     numberValueBlockEvaluator(numberValueBlock)
+     numberValueBlockEvaluator(numberValueBlock);
 
       expect( numberValueBlock.getFieldValue ).toHaveBeenCalledWith('input_value');
     })
@@ -21,15 +70,15 @@ describe("Blockly.Javascript", function() {
     it("converts the blocks input to an integer if its a string", function() {
       spyOn(numberValueBlock, "getFieldValue").and.returnValue('10');
 
-      var returnedValue = numberValueBlockEvaluator(numberValueBlock)
+      var returnedValue = numberValueBlockEvaluator(numberValueBlock);
 
       expect( returnedValue[0] ).toEqual(10);
     })
 
     it("does not have any order dependencies", function() {
-      var returnedValue = numberValueBlockEvaluator(numberValueBlock)
+      var returnedValue = numberValueBlockEvaluator(numberValueBlock);
 
       expect( returnedValue[1] ).toEqual(Blockly.JavaScript.ORDER_NONE);
-    })
-  })
-})
+    });
+  });
+});
---
+++ resources/public/sphero/commander.js
@@ -0,0 +1,25 @@
+function Commander(httpClient) {
+  this.httpClient = httpClient;
+
+  return this;
+}
+
+Commander.prototype.forward = function(options) {
+  this.sendCommand("forward", options);
+}
+
+Commander.prototype.reverse = function(options) {
+  this.sendCommand("reverse", options);
+}
+
+Commander.prototype.left = function(options) {
+  this.sendCommand("left", options);
+}
+
+Commander.prototype.right = function(options) {
+  this.sendCommand("right", options);
+}
+
+Commander.prototype.sendCommand = function(commandName, options) {
+  this.httpClient.get("/command/" + commandName + "?speed=" + options.speed);
+}
