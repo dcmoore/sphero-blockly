bf0e01344662ce7807b3744c5929ab71b7db0d27
Dave Moore <dave@8thlight.com>
2015-06-24 19:47:31 -0700

    Added support for distance parameter

--- src/sphero_blockly/commands/handler.clj
+++ src/sphero_blockly/commands/handler.clj
@@ -4,11 +4,15 @@
             [compojure.route :as route]
             [sphero-blockly.commands.executer :refer [move-direction]]))
 
-(defn- get-speed-from [request]
-  (:speed (:params request)))
+(defn- move [direction request]
+  (move-direction direction
+                  (:speed (:params request))
+                  (:distance (:params request))))
+
+(def success-response {:status 200 :body "Great job!!"})
 
 (defroutes command-handler
-  (GET "/forward" request (move-direction :forward (get-speed-from request)))
-  (GET "/reverse" request (move-direction :reverse (get-speed-from request)))
-  (GET "/left" request (move-direction :left (get-speed-from request)))
-  (GET "/right" request (move-direction :right (get-speed-from request))))
+  (GET "/forward" request (do (move :forward request) success-response))
+  (GET "/reverse" request (do (move :reverse request) success-response))
+  (GET "/left" request (do (move :left request) success-response))
+  (GET "/right" request (do (move :right request) success-response)))
--- test/sphero_blockly/commands/handler_test.clj
+++ test/sphero_blockly/commands/handler_test.clj
@@ -10,27 +10,27 @@
   (testing "forward"
     (let [move-direction-invocations (atom [])]
       (with-redefs [move-direction (mock-fn move-direction-invocations "{}")]
-        (let [response (command-handler (assoc (mock/request :get "/forward") :params {:speed "1"}))]
+        (let [response (command-handler (assoc (mock/request :get "/forward") :params {:speed "1" :distance "2"}))]
           (is (= (:status response) 200))
-          (is (= @move-direction-invocations [[:forward "1"]]))))))
+          (is (= @move-direction-invocations [[:forward "1" "2"]]))))))
 
   (testing "reverse"
     (let [move-direction-invocations (atom [])]
       (with-redefs [move-direction (mock-fn move-direction-invocations "{}")]
-        (let [response (command-handler (assoc (mock/request :get "/reverse") :params {:speed "5040"}))]
+        (let [response (command-handler (assoc (mock/request :get "/reverse") :params {:speed "5040" :distance "23"}))]
           (is (= (:status response) 200))
-          (is (= @move-direction-invocations [[:reverse "5040"]]))))))
+          (is (= @move-direction-invocations [[:reverse "5040" "23"]]))))))
 
   (testing "left"
     (let [move-direction-invocations (atom [])]
       (with-redefs [move-direction (mock-fn move-direction-invocations "{}")]
-        (let [response (command-handler (assoc (mock/request :get "/left") :params {:speed "11"}))]
+        (let [response (command-handler (assoc (mock/request :get "/left") :params {:speed "11" :distance "7"}))]
           (is (= (:status response) 200))
-          (is (= @move-direction-invocations [[:left "11"]]))))))
+          (is (= @move-direction-invocations [[:left "11" "7"]]))))))
 
   (testing "right"
     (let [move-direction-invocations (atom [])]
       (with-redefs [move-direction (mock-fn move-direction-invocations "{}")]
-        (let [response (command-handler (assoc (mock/request :get "/right") :params {:speed "5318008"}))]
+        (let [response (command-handler (assoc (mock/request :get "/right") :params {:speed "5318008" :distance "07734"}))]
           (is (= (:status response) 200))
-          (is (= @move-direction-invocations [[:right "5318008"]])))))))
+          (is (= @move-direction-invocations [[:right "5318008" "07734"]])))))))
