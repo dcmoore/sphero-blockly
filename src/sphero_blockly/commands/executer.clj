c8a90e85d5bfbb15734a1c3381017e7364681838
Dave Moore <dave@8thlight.com>
2015-06-24 20:34:17 -0700

    hackity sax

--- src/sphero_blockly/commands/executer.clj
+++ src/sphero_blockly/commands/executer.clj
@@ -1,4 +1,26 @@
-(ns sphero-blockly.commands.executer)
+(ns sphero-blockly.commands.executer
+  (:require [ellipso.core :as core]
+            [ellipso.commands :as commands]))
+
+(def sphero (atom nil))
+
+(defn- ensure-connection []
+  (when (nil? @sphero)
+    (reset! sphero (core/connect "/dev/tty.Sphero-GYG-AMP-SPP"))))
+
+(defn- numeric-direction [direction]
+  (case direction
+    :forward 0
+    :right 90
+    :reverse 180
+    :left 270))
 
 (defn move-direction [direction speed distance]
-  (str speed))
+  (ensure-connection)
+  (let [distance (* (read-string distance) 100)
+        speed (read-string speed)
+        speed (if (< speed 256) speed 255)
+        direction (numeric-direction direction)]
+    (commands/execute @sphero (commands/roll speed direction))
+    (Thread/sleep distance)
+    (commands/execute @sphero (commands/roll 0 0))))
