377a1e11db90ac54cc8d3262db718c3a5a949051
Zach Olauson <zmolauson@gmail.com>
2015-06-24 20:32:13 -0700

    Made the number_value blocks yellow

--- resources/public/common/blocks/common.js
+++ resources/public/common/blocks/common.js
@@ -1,6 +1,6 @@
 Blockly.Blocks['number_value'] = {
   init: function() {
-    this.setColour(210);
+    this.setColour(65);
     this.appendDummyInput()
         .appendField(new Blockly.FieldTextInput(""), "input_value");
     this.setOutput(true, "Number");
