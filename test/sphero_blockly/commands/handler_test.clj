691fce4d922193a635236d7e23ad9b99afcc52fe
Daveloper <daveloper@Davids-MacBook-Pro.local>
2016-01-26 21:05:52 -0800

    Added a test for reset-heading

--- test/sphero_blockly/commands/handler_test.clj
+++ test/sphero_blockly/commands/handler_test.clj
@@ -2,33 +2,40 @@
   (:require [clojure.java.io :as io]
             [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [sphero-blockly.commands.executer :refer [move-direction]]
+            [sphero-blockly.commands.executer :refer [move-direction reset-heading]]
             [sphero-blockly.commands.handler :refer :all]
-            [sphero-blockly.test-helper :refer [mock-fn]]))
+            [sphero-blockly.test-helper :refer [check-if-invoked-fn record-invocations-fn]]))
 
 (def move-direction-invocations (atom []))
-(defmacro with-setup-testing [docstring test-body]
+(defmacro testing-with-mock-move-direction [docstring test-body]
   `(reset! move-direction-invocations [])
-  `(with-redefs [move-direction (mock-fn move-direction-invocations "{}")]
+  `(with-redefs [move-direction (record-invocations-fn move-direction-invocations)]
     (testing `docstring `test-body)))
 
-(deftest test-app
-  (with-setup-testing "forward"
+(deftest test-maps-requests-to-the-correct-command
+  (testing-with-mock-move-direction "forward command"
     (let [response (command-handler (assoc (mock/request :get "/forward") :params {:speed "1" :distance "2"}))]
       (is (= (:status response) 200))
       (is (= @move-direction-invocations [[:forward "1" "2"]]))))
 
-  (with-setup-testing "reverse"
+  (testing-with-mock-move-direction "reverse command"
     (let [response (command-handler (assoc (mock/request :get "/reverse") :params {:speed "5040" :distance "23"}))]
       (is (= (:status response) 200))
       (is (= @move-direction-invocations [[:reverse "5040" "23"]]))))
 
-  (with-setup-testing "left"
+  (testing-with-mock-move-direction "left command"
     (let [response (command-handler (assoc (mock/request :get "/left") :params {:speed "11" :distance "7"}))]
       (is (= (:status response) 200))
       (is (= @move-direction-invocations [[:left "11" "7"]]))))
 
-  (with-setup-testing "right"
+  (testing-with-mock-move-direction "right command"
     (let [response (command-handler (assoc (mock/request :get "/right") :params {:speed "5318008" :distance "07734"}))]
       (is (= (:status response) 200))
-      (is (= @move-direction-invocations [[:right "5318008" "07734"]])))))
+      (is (= @move-direction-invocations [[:right "5318008" "07734"]]))))
+
+  (testing "reset-heading command"
+    (let [called-reset-heading? (atom false)]
+      (with-redefs [reset-heading (check-if-invoked-fn called-reset-heading?)]
+        (let [response (command-handler (mock/request :get "/reset-heading"))]
+          (is (= (:status response) 200))
+          (is @called-reset-heading?))))))
